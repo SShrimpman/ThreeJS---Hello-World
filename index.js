b78a4fc929de1e1af8294aa8c29792a456068827
SShrimpman <camaraoacsn@gmail.com>
2023-02-14 09:08:48 +0000

    Camera Controls

--- index.js
+++ index.js
@@ -4,14 +4,47 @@ import {
     MeshBasicMaterial,
     Mesh,
     PerspectiveCamera,
-    WebGLRenderer
+    WebGLRenderer,
+    MOUSE,
+    Vector2,
+    Vector3,
+    Vector4,
+    Quaternion,
+    Matrix4,
+    Spherical,
+    Box3,
+    Sphere,
+    Raycaster,
+    MathUtils,
+    Clock
 } from 'three';
 
+import  CameraControls  from 'camera-controls';
+
+const subsetOfTHREE = {
+    MOUSE,
+    Vector2,
+    Vector3,
+    Vector4,
+    Quaternion,
+    Matrix4,
+    Spherical,
+    Box3,
+    Sphere,
+    Raycaster,
+    MathUtils: {
+        DEG2RAD: MathUtils.DEG2RAD,
+        clamp: MathUtils.clamp,
+    }
+};
+
 // 1 the Scene
+
 const scene = new Scene();
 const canvas = document.getElementById('three-canvas');
 
 // 2 the Object
+
 const geometry = new BoxGeometry( 0.5, 0.5, 0.5);
 const orangeMaterial = new MeshBasicMaterial( { color: 'orange' } );
 const blueMaterial = new MeshBasicMaterial( { color: 'blue' } );
@@ -25,16 +58,13 @@ bigBlueCube.scale.set(2, 2, 2);
 scene.add( bigBlueCube );
 
 // 3 the Camera
-const sizes = {
-    width: 800,
-    height: 600
-}
 
 const camera = new PerspectiveCamera(75, canvas.clientWidth / canvas.clientHeight);
 camera.position.z = 3;
 scene.add(camera);
 
 // 4 the Renderer
+
 const renderer = new WebGLRenderer({ canvas });
 renderer.setPixelRatio( Math.min(window.devicePixelRatio, 2) );
 renderer.setSize( canvas.clientWidth, canvas.clientHeight, false );
@@ -48,17 +78,21 @@ window.addEventListener('resize', () => {
     
 })
 
-// 6 Animation
+// 6 Controls
+
+CameraControls.install( { THREE: subsetOfTHREE } );
+const clock = new Clock();
+const cameraControls = new CameraControls( camera, canvas );
+cameraControls.dollyToCursor = true;
+
+
+// 7 Animation
 
 function animate() {
-    orangeCube.rotation.x += 0.01;
-    orangeCube.rotation.z += 0.01;
-
-    bigBlueCube.rotation.x -= 0.02;
-    bigBlueCube.rotation.z -= 0.02;
+    const delta = clock.getDelta();
+    cameraControls.update( delta );
 
     renderer.render( scene, camera );
     requestAnimationFrame( animate );
 }
-
 animate();
